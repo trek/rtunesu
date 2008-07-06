743d30cac1d79fb67cc566aa43b6cf8275603532
Trek Glowacki <trek@neoptolemus.local>
2008-07-06 12:28:18 -0400

    Added some ability to drill down into returned xml. May take another approach

--- lib/rtunesu/entity.rb
+++ lib/rtunesu/entity.rb
@@ -1,5 +1,6 @@
 module RTunesU
   class Entity
+    NESTING = %w(Site Section Course Group)
     attr_accessor :connection
     
     def initialize(attrs)
@@ -12,8 +13,9 @@ module RTunesU
     end
     
     def load_from_xml(xml)
-      XmlSimple.xml_in(xml, 'ForceArray' => false)['Site'][self.class_name]
+      self.drill(self.class_name, XmlSimple.xml_in(xml, 'ForceArray' => false))
     end
+    
     
     # 'attributes' are a specific type of instance data that is accessible with either the getter methods or through the attributes hash.  Only data listed as an 'attribute' will serialize to xml when saving to iTunes U.  This allows Entity objects to have local data that will not be send to iTunes U
     # 'attributes' are listed at the beginning of a class defintion and define setters, getters, and the attributes hash.
@@ -51,5 +53,12 @@ module RTunesU
         self.attributes.each { |name, value| xml_builder.tag!(name.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }, value) unless value.nil?}
       }
     end
+  
+    protected
+      # iTunes U always returns an entity nested inside of its hierarchical entities.  For example requesting a Course entity will return an XML document with Course nested inside its Section and Site.
+      # Drill takes a hash converted from this XML using load_from_xml and recursively moves inwards until it finds the entity type being searched for
+      def drill(level, hash, index = 0)
+        level == NESTING[index] ? hash.fetch(NESTING[index]) : drill(level, hash.fetch(NESTING[index]), index += 1)
+      end
   end
 end

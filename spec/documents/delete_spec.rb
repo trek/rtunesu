29f8d5e770946ae32d643d420d82b2a0c45f00a4
Trek Glowacki <trek@neoptolemus.local>
2008-07-12 15:27:14 -0400

    Removed the rest of spec dependencies on SimpleXml

--- spec/documents/delete_spec.rb
+++ spec/documents/delete_spec.rb
@@ -16,15 +16,15 @@ describe Document::Delete do
     before do
       course = Course.new(:handle => 1234567, :parent_handle =>  98765)
       document = Document::Delete.new(course)
-      @xml = XmlSimple.xml_in(@document.xml, 'KeepRoot' => true, 'ForceArray' => false)
+      @xml = Hpricot.XML(document.xml)
     end
 
     it 'has a parent handle' do
-      @xml['ITunesUDocument']['DeleteCourse'].should have_key('CourseHandle')
+      @xml.at('DeleteCourse/CourseHandle').should_not be_nil
     end
   
     it 'has a parent path' do
-      @xml['ITunesUDocument']['DeleteCourse'].should have_key('CoursePath')
+      @xml.at('DeleteCourse/CoursePath').should_not be_nil
     end
   end
 end
--- spec/documents/merge_spec.rb
+++ spec/documents/merge_spec.rb
@@ -16,15 +16,15 @@ describe Document::Merge do
     before do
       course = Course.new(:handle => 1234567, :parent_handle =>  98765)
       document = Document::Merge.new(course)
-      @xml = XmlSimple.xml_in(@document.xml, 'KeepRoot' => true, 'ForceArray' => false)
+      @xml = Hpricot.XML(document.xml)
     end
 
     it 'has a handle that represents its entity' do
-      @xml['ITunesUDocument']['MergeCourse'].should have_key('CourseHandle')
+      @xml.at('MergeCourse/CourseHandle').should_not be_nil
     end
   
     it 'has a child element that represents the source entity' do
-      @xml['ITunesUDocument']['MergeCourse'].should have_key('Course')
+      @xml.at('MergeCourse/Course').should_not be_nil
     end
   end
 end

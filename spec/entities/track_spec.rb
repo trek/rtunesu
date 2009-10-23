80e746e78e480c1c0b5717c60705533a98880369
Trek Glowacki <pietrekg@umich.edu>
2009-10-23 16:09:23 -0400

    spec for unique finding behavior of Course

--- spec/entities/track_spec.rb
+++ spec/entities/track_spec.rb
@@ -16,4 +16,22 @@ describe Track do
   # it_should_behave_like "a deleteable Entity"
   it_should_behave_like "an Entity with attribute assignment"
   
+  describe "finding" do
+    before(:each) do
+      mock_connect!
+      FakeWeb.register_uri(:get,
+                           "https://deimos.apple.com/WebObjects/Core.woa/API/GetUploadURL/example.edu.2394598528?credentials=Administrator%40urn%3Amace%3Aitunesu.com%3Asites%3Aexample.edu&identity=%22Admin%22+%3Cadmin%40example.edu%3E+%28admin%29+%5B0%5D&time=1214619134&signature=121a6cf76c9c5ecda41450d87e3394b9d02c570a5f76b2bd16287f860f068302&type=XMLControlFile",
+                           :body => mock_upload_url_for_handle(1))
+      FakeWeb.register_uri(:post, mock_upload_url_for_handle(1), :body => response_for(Course, 'show', true))
+    end
+    
+    after(:each) do
+      FakeWeb.clean_registry
+    end
+    
+    it "should have an handle" do
+      lambda { Track.find(2449134252, 2394598528) }.should_not raise_error
+    end
+    
+  end
 end

3c2ec3372b2f77d7f8d6c6a5bf027d485c852898
Trek Glowacki <pietrekg@umich.edu>
2009-10-27 13:26:56 -0400

    Fixed bug where Sections could be updated but not created

--- lib/rtunesu/entities/section.rb
+++ lib/rtunesu/entities/section.rb
@@ -8,11 +8,13 @@ module RTunesU
     
     # Sections have additional required attributes for updating. These attributes *must* appear
     # in a specific order.
-    # see http://deimos.apple.com/rsrc/doc/iTunesUAdministratorsGuide/iTunesUWebServices/chapter_17_section_12.html
+    # see http://deimos.apple.com/rsrc/doc/iTunesUAdministratorsGuide/iTunesUWebServices/chapter_17_section_12.html    
     def to_xml(xml_builder = Builder::XmlMarkup.new)
-      xml_builder.tag!("SectionPath", '')
-      xml_builder.tag!("MergeByHandle", "false")
-      xml_builder.tag!("Destructive", "false")
+      if saved?
+        xml_builder.tag!("SectionPath", '')
+        xml_builder.tag!("MergeByHandle", "false")
+        xml_builder.tag!("Destructive", "false")
+      end
       super
     end
   end

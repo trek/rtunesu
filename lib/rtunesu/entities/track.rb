4a679be49e1310ad727cc3edb191d3ef6cfb868e
Trek Glowacki <pietrekg@umich.edu>
2009-10-27 22:48:18 -0400

    Tracks can now be created with data or from a file

--- lib/rtunesu/entities/track.rb
+++ lib/rtunesu/entities/track.rb
@@ -1,18 +1,41 @@
 module RTunesU
   class Track < Entity
-    composed_of :name, :kind, :disc_number, :album_name, :arist_name, :category_code, :explicit
+    attr_accessor :file
+    composed_of :name, :kind, :disc_number, :track_number, 
+                :album_name, :arist_name, :category_code, :explicit,
+                :genre_name, :comment
+                
     composed_of :duration_in_milliseconds, :as => 'DurationMilliseconds', :readonly => true
     composed_of :download_url, :as => 'DownloadURL', :readonly => true
+    has_a :remote_file, :as => 'File', :readonly => true
+    
+    
+    def create(connection = nil)
+      connection ||= self.base_connection
+      if file
+        response = connection.upload_file(file, self.parent_handle, false)
+        @parent_handle, @handle = response.split('.')[-2..-1]
+        self.source_xml = Group.find(@parent_handle, connection).source_xml.at("Track/Handle[text()=#{@handle}]/..")
+        self
+      else
+        super
+      end
+    end
+    
+    def update(connection = nil)
+      raise "Cannot save existing Track with new file attached. Delete the old track and create a new one instead."  if file
+      super
+    end
     
     # Tracks can only be found from within their Course. 
     # There is currently (v1.1.3) no way to find a Track separete from its Course in iTunes U.
-    def self.find(handle, course_handle, connection = nil)
+    def self.find(handle, group_handle, connection = nil)
       connection ||= self.base_connection
       
-      entity = self.new
-      entity.instance_variable_set('@handle', handle)
-      entity.source_xml = Course.find(course_handle, connection).source_xml.at("Handle[text()=#{entity.handle}]..")
-      entity
+      track = self.new
+      track.instance_variable_set('@handle', handle)
+      track.source_xml = Group.find(group_handle, connection).source_xml.at("Track/Handle[text()=#{handle}]/..")
+      track
     end
   end
 end
--- spec/entities/track_spec.rb
+++ spec/entities/track_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper.rb'
 require File.dirname(__FILE__) + '/../entity_spec.rb'
-
+require 'tempfile'
 include RTunesU
 
 describe Track do
@@ -11,21 +11,78 @@ describe Track do
     @attributes = {:name => 'Sample Lecture'}
   end
   
-  it_should_be_composed_of :name, :kind, :disc_number, :album_name, :arist_name, :category_code, :explicit
-  it_should_be_composed_of_readonly :duration_in_milliseconds, :download_url
+  it_should_be_composed_of :name, :kind, :disc_number, :track_number, :album_name, :arist_name, :category_code, :explicit, :genre_name, :comment  
+  it_should_be_composed_of :duration_in_milliseconds, :download_url, :readonly => true
+  it_should_have_a :remote_file 
   
   # it_should_behave_like "a creatable Entity"
   # it_should_behave_like "an updateable Entity"
   # it_should_behave_like "a deleteable Entity"
   it_should_behave_like "an Entity with attribute assignment"
   
+  describe "creating" do
+    before(:each) do
+      @group_handle = 2628591672
+      @track_handle = 2681543745
+      mock_connect!
+    end
+    
+    describe "without local file" do
+      
+    end
+    
+    describe "with local file " do
+      before(:each) do
+        # Fake call to create upload url for Group
+        FakeWeb.register_uri(:get,
+                             "https://deimos.apple.com/WebObjects/Core.woa/API/GetUploadURL/example.edu.#{@group_handle}?credentials=Administrator%40urn%3Amace%3Aitunesu.com%3Asites%3Aexample.edu&identity=%22Admin%22+%3Cadmin%40example.edu%3E+%28admin%29+%5B0%5D&time=1214619134&signature=121a6cf76c9c5ecda41450d87e3394b9d02c570a5f76b2bd16287f860f068302",
+                             :body => mock_upload_url_for_handle(@group_handle)
+                            )
+        # Fake call to upload file to Group, then to upload ShowTree to group again
+        FakeWeb.register_uri(:post, 
+                              mock_upload_url_for_handle(@group_handle),
+                              [
+                               {:body => response_for(Track, 'create_with_file', true)},
+                               {:body => response_for(Group, 'show', true)}
+                              ]
+                            )
+
+        # Fake call to create upload url for Group, again for ShowTree on the Group
+        FakeWeb.register_uri(:get,
+                             "https://deimos.apple.com/WebObjects/Core.woa/API/GetUploadURL/example.edu.#{@group_handle}?credentials=Administrator%40urn%3Amace%3Aitunesu.com%3Asites%3Aexample.edu&identity=%22Admin%22+%3Cadmin%40example.edu%3E+%28admin%29+%5B0%5D&time=1214619134&signature=121a6cf76c9c5ecda41450d87e3394b9d02c570a5f76b2bd16287f860f068302&type=XMLControlFile",
+                             :body => mock_upload_url_for_handle(@group_handle)
+        )
+                            
+        @track.parent_handle = @group_handle
+        @track.file = fixture_file('sounds-of-the-cafe.mov')
+      end
+      
+      it "should save to iTunes" do
+        lambda { @track.save}.should_not raise_error
+      end
+      
+      it "should have track meta data from iTunesU" do
+        @track.save
+        @track.name.should == "vidtest"
+        @track.duration_in_milliseconds.should == "1000"
+      end
+      
+      it "should raise error if attempting to update" do
+        @track.instance_variable_set("@handle", 1)
+        lambda { @track.save}.should raise_error
+      end
+    end
+  end
+  
   describe "finding" do
     before(:each) do
+      @group_handle = 2628591672
+      @track_handle = 2681543745
       mock_connect!
       FakeWeb.register_uri(:get,
-                           "https://deimos.apple.com/WebObjects/Core.woa/API/GetUploadURL/example.edu.2394598528?credentials=Administrator%40urn%3Amace%3Aitunesu.com%3Asites%3Aexample.edu&identity=%22Admin%22+%3Cadmin%40example.edu%3E+%28admin%29+%5B0%5D&time=1214619134&signature=121a6cf76c9c5ecda41450d87e3394b9d02c570a5f76b2bd16287f860f068302&type=XMLControlFile",
-                           :body => mock_upload_url_for_handle(1))
-      FakeWeb.register_uri(:post, mock_upload_url_for_handle(1), :body => response_for(Course, 'show', true))
+                           "https://deimos.apple.com/WebObjects/Core.woa/API/GetUploadURL/example.edu.#{@group_handle}?credentials=Administrator%40urn%3Amace%3Aitunesu.com%3Asites%3Aexample.edu&identity=%22Admin%22+%3Cadmin%40example.edu%3E+%28admin%29+%5B0%5D&time=1214619134&signature=121a6cf76c9c5ecda41450d87e3394b9d02c570a5f76b2bd16287f860f068302&type=XMLControlFile",
+                           :body => mock_upload_url_for_handle(@group_handle))
+      FakeWeb.register_uri(:post, mock_upload_url_for_handle(@group_handle), :body => response_for(Group, 'show', true))
     end
     
     after(:each) do
@@ -33,11 +90,11 @@ describe Track do
     end
     
     it "should be found via its course" do
-      lambda { Track.find(2449134252, 2394598528) }.should_not raise_error
+      lambda { Track.find(@track_handle, @group_handle) }.should_not raise_error
     end
     
     it "should have a handle" do
-      Track.find(2449134252, 2394598528).handle.should_not == nil
+      Track.find(@track_handle, @group_handle).handle.should_not == nil
     end
   end
 end

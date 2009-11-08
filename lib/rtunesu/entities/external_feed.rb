cb0976c9cf64ae902684fbba2c0859dfeb14fabe
Trek Glowacki <pietrekg@umich.edu>
2009-11-08 13:20:27 -0500

    Group and ExternalFeed objects require specific serialization order. NOTE: this is retarded.

--- lib/rtunesu/entities/external_feed.rb
+++ lib/rtunesu/entities/external_feed.rb
@@ -5,6 +5,15 @@ module RTunesU
     validates!  :polling_interval, [:never, :daily]
     validates!  :security_type,  [:none, 'Basic Authentication']
     
+    # an ExternalFeed must serialize in a specific order
+    def to_xml(xml_builder = Builder::XmlMarkup.new)
+      xml_builder.tag!('ExternalFeed') do
+        %w(URL OwnerEmail PollingInterval SecurityType SignatureType BasicAuthUsername BasicAuthPassword).each do |tag_name|
+          xml_builder.tag!(tag_name, self.edits[tag_name]) if self.edits[tag_name]
+        end
+      end
+    end
+    
     # As an entirely nested entity ExternalFeed instances don't have a parent handle. However, something 'truthy' needs
     # to be presnet for proper XML document genration.
     def parent_handle # :nodoc:
--- lib/rtunesu/entities/group.rb
+++ lib/rtunesu/entities/group.rb
@@ -10,11 +10,30 @@ module RTunesU
       :shared    => "Shared",
       :edit      => "Edit"
     }
+    TYPES = {
+      :feed => "Feed"
+    }
     composed_of :name, :group_type, :short_name, :allow_subscription, :explicit
     composed_of :aggregate_file_size, :readonly => true
     validates! :group_type, [:simple, :smart, :feed]
     has_a :external_feed
     has_n :permissions, :tracks, :shared_objects
     
+    # a Group must serialize in a specific order
+    def to_xml(xml_builder = Builder::XmlMarkup.new)
+      xml_builder.tag!('Group') do
+        %w(Name Handle GroupType Explicit).each do |tag_name|
+          xml_builder.tag!(tag_name, self.edits[tag_name]) if self.edits[tag_name]
+        end
+        
+        self.tracks.to_xml(xml_builder)
+        self.permissions.to_xml(xml_builder)
+        
+        %w(AllowSubscription).each do |tag_name|
+          xml_builder.tag!(tag_name, self.edits[tag_name]) if self.edits[tag_name]
+        end
+        self.external_feed.to_xml(xml_builder) if self.external_feed
+      end
+    end
   end
 end
